bdae725967f8aafa83bbf1421dc6a8e469cea066
ramalho-cmd <rapharam96@gmail.com>
2025-03-05 17:34:09 -0300

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -12,3 +12,16 @@ AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/auth"
 TOKEN_URL = "https://oauth2.googleapis.com/token"
 USERINFO_URL = "https://www.googleapis.com/oauth2/v1/userinfo"
 
+# Escopos solicitados
+SCOPES = ["openid", "email", "profile"]
+
+# Título do app
+st.title("Login com Google no Streamlit")
+
+
+if st.button("Login com Google"):
+        oauth = OAuth2Session(CLIENT_ID, CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=SCOPES)
+        auth_url, state = oauth.create_authorization_url(AUTHORIZATION_URL)
+
+        # Redireciona o usuário para a URL de login do Google
+        st.write(f"[Clique aqui para fazer login]({auth_url})") 
