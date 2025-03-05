b889106a40ac8bbe3a06b899b6b9a88ff9f07520
ramalho-cmd <rapharam96@gmail.com>
2025-03-05 20:43:57 -0300

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -5,12 +5,14 @@ from dotenv import load_dotenv
 
 
 #Configurações OAuth2 do Google
+load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = "http://localhost:8501/"  # URL de redirecionamento do OAuth2
 AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/auth"
 TOKEN_URL = "https://oauth2.googleapis.com/token"
 USERINFO_URL = "https://www.googleapis.com/oauth2/v1/userinfo"
+
 
 # Escopos solicitados
 SCOPES = ["openid", "email", "profile"]
@@ -26,6 +28,7 @@ if st.button("Login com Google"):
         # Redireciona o usuário para a URL de login do Google
         st.write(f"[Clique aqui para fazer login]({auth_url})") 
 
+# Captura parâmetros da URL (para quando o Google redireciona de volta com o código de autorização)
 
 query_params = st.query_params
 auth_code = query_params.get("code", None)
